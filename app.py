2e248b19137f421f1df0db923856f5ae517b05ee
Kevin Cho <takearest118@gmail.com>
2016-10-10 17:12:00 +0900

    added settings

--- app.py
+++ app.py
@@ -4,26 +4,27 @@
 import logging
 
 from tornado import web, ioloop, httpserver
+
 from urls import url_patterns
 
 
 class APIApplication(web.Application):
     def __init__(self, *args, **kwargs):
-        config = dict()
-        super(APIApplication, self).__init__(url_patterns, **config)
-        pass
+        config = kwargs.get('config', None)
+        super(APIApplication, self).__init__(url_patterns, **config['application'])
+
 
 def main():
-    #import settings
-    #config = settings.settings()
+    import settings
+    config = settings.settings()
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger('coconut')
     try:
-        app = APIApplication()
+        app = APIApplication(config=config)
         server = httpserver.HTTPServer(app, xheaders=True)
-        server.bind(8888)
+        server.bind(config['application']['port'])
         server.start(1)
-        logger.info('api start up on Tornado listen %d' % 8888)
+        logger.info('api start up on Tornado listen %d' % config['application']['port'])
         ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
         logger.info('KeyboardInterrupt occured. Stopping instance')
---
+++ settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -8-
+
+
+_settings = dict(
+    application=dict(
+        name='coconut',
+        port=5100,
+        debug=True,
+        autoreload=True
+    )
+)
+
+
+def settings():
+    return _settings
