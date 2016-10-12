7e51b7b05cf4baa284743e268b6653a7e6c4c30d
Kevin Cho <takearest118@gmail.com>
2016-10-12 18:58:29 +0900

    updated logging format

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
-import logging
+from tornado.log import logging
 
 from tornado import web, ioloop, httpserver
+from tornado.options import parse_command_line
 
 from urls import url_patterns
 
@@ -11,14 +12,15 @@ from urls import url_patterns
 class APIApplication(web.Application):
     def __init__(self, *args, **kwargs):
         config = kwargs.get('config', None)
+        parse_command_line()
         super(APIApplication, self).__init__(url_patterns, **config['application'])
 
 
 def main():
     import settings
     config = settings.settings()
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger('coconut')
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(name)s:%(module)s %(message)s')
+    logger = logging.getLogger(config['application']['name'])
     try:
         app = APIApplication(config=config)
         server = httpserver.HTTPServer(app, xheaders=True)
