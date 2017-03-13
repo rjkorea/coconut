7d12e8a983896482646d3a103a3ecdf45c8b3e1a
Kevin Cho <takearest118@gmail.com>
2017-03-13 15:41:26 +0900

    add query parameter on handler of admins

--- handlers/a/admin.py
+++ handlers/a/admin.py
@@ -11,11 +11,23 @@ from models.admin import AdminModel
 
 class AdminListHandler(JsonHandler):
     @admin_auth_async
-    @parse_argument([('start', int, 0), ('size', int, 10), ])
+    @parse_argument([('start', int, 0), ('size', int, 10), ('q', str, None)])
     async def get(self, *args, **kwargs):
         parsed_args = kwargs.get('parsed_args')
-        count = await AdminModel.count()
-        result = await AdminModel.find(skip=parsed_args['start'], limit=parsed_args['size'])
+        if parsed_args['q']:
+            q = {
+                '$or': [
+                    {'name': {'$regex': parsed_args['q']}},
+                    {'email': {'$regex': parsed_args['q']}},
+                    {'company': {'$regex': parsed_args['q']}},
+                    {'mobile_number': {'$regex': parsed_args['q']}},
+                    {'role': {'$regex': parsed_args['q']}}
+                ]
+            }
+        else:
+            q = {}
+        count = await AdminModel.count(query=q)
+        result = await AdminModel.find(query=q, skip=parsed_args['start'], limit=parsed_args['size'])
         self.response['data'] = result
         self.response['count'] = count
         self.write_json()
