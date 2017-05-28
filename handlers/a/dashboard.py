12022b9cdcfa3f86834892db9d7c1f7cf81668f3
Kevin Cho <takearest118@gmail.com>
2017-05-28 20:20:14 +0900

    update dashboard api for super role admin

--- handlers/a/dashboard.py
+++ handlers/a/dashboard.py
@@ -5,19 +5,24 @@ from tornado.web import HTTPError
 from common.decorators import admin_auth_async
 
 from handlers.base import JsonHandler
-from models.invitation import InvitationModel
+from models.company import CompanyModel
+from models.user import UserModel
+from models.content import ContentModel
+from models.ticket import TicketModel
 
 
 class DashboardHandler(JsonHandler):
     @admin_auth_async
     async def get(self, *args, **kwargs):
-        total_users_count = await InvitationModel.count({'enabled': True})
-        total_visitors_count = await InvitationModel.count({'enabled': True, 'entered': True})
-        total_visitors_fee_count = await InvitationModel.count({'enabled': True, 'entered': True, 'fee.enabled': True})
+        total_company_count = await CompanyModel.count({'enabled': True})
+        total_user_count = await UserModel.count({'enabled': True})
+        total_content_count = await ContentModel.count({'enabled': True})
+        total_ticket_count = await TicketModel.count({'enabled': True})
         self.response['data'] = {
-            'total_users_count': total_users_count,
-            'total_visitors_count': total_visitors_count,
-            'total_visitors_fee_count': total_visitors_fee_count
+            'total_ticket_count': total_ticket_count,
+            'total_company_count': total_company_count,
+            'total_user_count': total_user_count,
+            'total_content_count': total_content_count
         }
         self.write_json()
 
