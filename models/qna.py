eb120a548e95e679aa127b98a22c35146f5b5b05
Kevin Cho <takearest118@gmail.com>
2017-06-15 16:38:13 +0900

    add qna model

---
+++ models/qna.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from bson import ObjectId
+
+from models.base import BaseModel
+
+
+class QnaModel(BaseModel):
+    MONGO_COLLECTION = 'qna'
+
+    def __init__(self, *args, **kwargs):
+        super(QnaModel, self).__init__(*args, **kwargs)
+
+    @property
+    def specification(self):
+        specification = super(QnaModel, self).specification
+        specification.extend([
+            {
+                'key': 'admin_oid',
+                'type': ObjectId,
+                'default': None
+            },
+            {
+                'key': 'company_oid',
+                'type': ObjectId,
+                'default': None
+            },
+            {
+                'key': 'content_oid',
+                'type': ObjectId,
+                'default': None
+            },
+            {
+                'key': 'question',
+                'type': str,
+                'default': None
+            },
+            {
+                'key': 'answer',
+                'type': str,
+                'default': None
+            },
+            {
+                'key': 'enabled',
+                'type': bool,
+                'default': (lambda: True)
+            },
+        ])
+        return specification
