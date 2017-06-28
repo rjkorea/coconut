de6ef52390364f5b4739d433f3c5625d8bf13ab4
Kevin Cho <takearest118@gmail.com>
2017-06-28 17:32:01 +0900

    add filter params on /w/qnas handler

--- handlers/w/qna.py
+++ handlers/w/qna.py
@@ -12,12 +12,14 @@ from common.decorators import user_auth_async, parse_argument
 
 class QnaListHandler(JsonHandler):
     @user_auth_async
-    @parse_argument([('start', int, 0), ('size', int, 10), ('content_oid', str, None)])
+    @parse_argument([('start', int, 0), ('size', int, 10), ('content_oid', str, None), ('filter', str, None)])
     async def get(self, *args, **kwargs):
         parsed_args = kwargs.get('parsed_args')
         q = {
             'content_oid': ObjectId(parsed_args['content_oid'])
         }
+        if parsed_args['filter'] == 'mytkit':
+            q['is_mytkit'] = True
         count = await QnaModel.count(query=q)
         result = await QnaModel.find(query=q, skip=parsed_args['start'], limit=parsed_args['size'])
         self.response['data'] = result
