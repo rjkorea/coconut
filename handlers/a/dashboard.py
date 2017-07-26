6fb366f21738bccbb0254d45e1cbd52a425ecfda
Kevin Cho <takearest118@gmail.com>
2017-07-26 14:59:49 +0900

    renewel content dashboard

--- handlers/a/dashboard.py
+++ handlers/a/dashboard.py
@@ -10,7 +10,7 @@ from handlers.base import JsonHandler
 from models.company import CompanyModel
 from models.user import UserModel
 from models.content import ContentModel
-from models.ticket import TicketModel, TicketTypeModel
+from models.ticket import TicketModel, TicketTypeModel, TicketOrderModel
 
 from services.mongodb import MongodbService
 
@@ -118,14 +118,21 @@ class DashboardContentHandler(JsonHandler):
                 'female': 0
             },
             'gender_count': {
-                'male': 0,
-                'female': 0
+                'register': {
+                    'male': 0,
+                    'female': 0
+                },
+                'use': {
+                    'male': 0,
+                    'female': 0
+                }
             },
             'revenue': {
                 'cash': 0,
                 'creditcard': 0
             },
-            'top_ticket_types': []
+            'top_ticket_types': [],
+            'top_ticket_orders': []
         }
         query = {
             'content_oid': ObjectId(content_oid),
@@ -183,7 +190,41 @@ class DashboardContentHandler(JsonHandler):
         ]
         res = await TicketModel.aggregate(pipeline, 5)
         for r in res:
-            self.response['data']['gender_count'][r['_id']] = r['count']
+            self.response['data']['gender_count']['use'][r['_id']] = r['count']
+
+        # register aggregate for genders
+        pipeline = [
+            {
+                '$match': {
+                    'content_oid': ObjectId(content_oid),
+                    'status': TicketModel.Status.register.name
+                }
+            },
+            {
+                '$lookup': {
+                    'from': 'user',
+                    'localField': 'receive_user_oid',
+                    'foreignField': '_id',
+                    'as': 'receive_user'
+                }
+            },
+            {
+                '$unwind': {
+                    'path': '$receive_user'
+                }
+            },
+            {
+                '$group': {
+                    '_id': '$receive_user.gender',
+                    'count': {
+                        '$sum': 1
+                    }
+                }
+            }
+        ]
+        res = await TicketModel.aggregate(pipeline, 5)
+        for r in res:
+            self.response['data']['gender_count']['register'][r['_id']] = r['count']
 
         # use aggregate for top ticket type
         pipeline = [
@@ -210,7 +251,37 @@ class DashboardContentHandler(JsonHandler):
         top_ticket_types = await TicketModel.aggregate(pipeline, 10)
         for ttt in top_ticket_types:
             ttt['ticket_type'] = await TicketTypeModel.get_id(ttt['_id'], fields=[('name'), ('desc')])
+            ttt['ticket_register_cnt'] = await TicketModel.count({'ticket_type_oid': ttt['_id'], 'status': TicketModel.Status.register.name})
         self.response['data']['top_ticket_types'] = top_ticket_types
+
+        # use aggregate for top ticket order
+        pipeline = [
+            {
+                '$match': {'content_oid': ObjectId(content_oid)}
+            },
+            {
+                '$group': {
+                    '_id': '$ticket_order_oid',
+                    'ticket_cnt': {
+                        '$sum': 1
+                    }
+                }
+            },
+            {
+                '$sort': {
+                    "ticket_cnt": -1
+                }
+            },
+            {
+                '$limit': 20
+            }
+        ]
+        top_ticket_orders = await TicketModel.aggregate(pipeline, 20)
+        for tto in top_ticket_orders:
+            tto['ticket_order'] = await TicketOrderModel.get_id(tto['_id'])
+            tto['ticket_type'] = await TicketTypeModel.get_id(tto['ticket_order']['ticket_type_oid'])
+            tto['ticket_register_cnt'] = await TicketModel.count({'ticket_order_oid': tto['_id'], 'status': TicketModel.Status.register.name})
+        self.response['data']['top_ticket_orders'] = top_ticket_orders
 
         #user aggregate for revenue
         pipeline = [
