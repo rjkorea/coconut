aaadda105b7d634dd4e99ca06ea660c0c1f8496a
Kevin Cho <takearest118@gmail.com>
2018-07-06 13:45:38 +0900

    add export user_ticket csvfile

--- scripts/export.py
+++ scripts/export.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 from bson import ObjectId
+from pprint import pprint
 
 import csv
 import click
@@ -17,7 +18,7 @@ def group_ticket():
 @click.option('-m', '--mongo', default='localhost:27017', help='host of mongodb')
 @click.option('-i', '--contentid', help='content id')
 @click.option('--dryrun', is_flag=True, help='dry run test')
-@click.confirmation_option(help='Are you sure you import to mongodb?')
+@click.confirmation_option(help='Are you sure you export to csv file?')
 def group_tickets(csvfile, mongo, contentid, dryrun):
     click.secho('= params info =', fg='cyan')
     click.secho('csvfile: %s' % (csvfile), fg='green')
@@ -48,7 +49,42 @@ def group_tickets(csvfile, mongo, contentid, dryrun):
         click.secho('check parameters <python export.py  --help>', fg='red')
 
 
-cli = click.CommandCollection(sources=[group_ticket])
+@click.group()
+def user_ticket():
+    pass
+
+@user_ticket.command()
+@click.option('-c', '--csvfile', help='csv filename')
+@click.option('-m', '--mongo', default='localhost:27017', help='host of mongodb')
+@click.option('-i', '--contentid', help='content id')
+@click.option('-s', '--status', help='status(send, register, pay, use, cancel')
+@click.option('--dryrun', is_flag=True, help='dry run test')
+@click.confirmation_option(help='Are you sure you import to mongodb?')
+def user_tickets(csvfile, mongo, contentid, status, dryrun):
+    click.secho('= params info =', fg='cyan')
+    click.secho('csvfile: %s' % (csvfile), fg='green')
+    click.secho('mongodb: %s' % (mongo), fg='green')
+    now = datetime.utcnow()
+    if csvfile and mongo and contentid and status:
+        fieldnames = ['name', 'mobile_number']
+        writer = csv.writer(open(csvfile, 'w'))
+        mongo_client = MongoClient(host=mongo.split(':')[0], port=int(mongo.split(':')[1]))
+        users_map = dict()
+        cursor = mongo_client['coconut']['ticket'].find({'content_oid': ObjectId(contentid), 'status': status})
+        while cursor.alive:
+            doc = cursor.next()
+            user = mongo_client['coconut']['user'].find_one({'_id': doc['receive_user_oid']})
+            users_map[user['mobile_number']] = user['name']
+        if dryrun:
+            pprint(users_map)
+        else:
+            for user in users_map.items():
+                writer.writerow([user[0].replace('8210', '010'), user[1]])
+    else:
+        click.secho('check parameters <python export.py  --help>', fg='red')
+
+
+cli = click.CommandCollection(sources=[group_ticket, user_ticket])
 
 
 if __name__ == '__main__':
