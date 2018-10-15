4371893a21959f45b67522e31628dff04d53d1d9
Kevin Cho <takearest118@gmail.com>
2018-10-15 14:47:41 +0900

    migration for birthday of user

--- scripts/migration.py
+++ scripts/migration.py
@@ -51,7 +51,51 @@ def ticket_types(mongo, dryrun):
         click.secho('check parameters <python migration.py --help>', fg='red')
 
 
-cli = click.CommandCollection(sources=[ticket_type])
+@click.group()
+def birthday():
+    pass
+
+@birthday.command()
+@click.option('-m', '--mongo', default='localhost:27017', help='host of mongodb')
+@click.option('--dryrun', is_flag=True, help='dry run test')
+@click.confirmation_option(help='Are you sure your migration?')
+def birthdays(mongo, dryrun):
+    click.secho('= params info =', fg='cyan')
+    click.secho('mongodb: %s' % (mongo), fg='green')
+    if mongo:
+        mongo_client = MongoClient(host=mongo.split(':')[0], port=int(mongo.split(':')[1]))
+        cursor = mongo_client['coconut']['user'].find({})
+        while cursor.alive:
+            doc = cursor.next()
+            set = dict()
+            if 'birthday' in doc and len(doc['birthday'].strip()) == 8:
+                set = {
+                    '$set': {
+                        'birthday': doc['birthday']
+                    }
+                }
+            elif 'birthday' in doc and len(doc['birthday'].strip()) == 6:
+                if doc['birthday'][0] == '0' or doc['birthday'][0] =='1':
+                    set = {
+                        '$set': {
+                            'birthday': '20%s' % doc['birthday']
+                        }
+                    }
+                else:
+                    set = {
+                        '$set': {
+                            'birthday': '19%s' % doc['birthday']
+                        }
+                    }
+            if dryrun:
+                pprint(set)
+            else:
+                mongo_client['coconut']['user'].update({'_id': doc['_id']}, set, False, False)
+    else:
+        click.secho('check parameters <python migration.py --help>', fg='red')
+
+
+cli = click.CommandCollection(sources=[ticket_type, birthday])
 
 
 if __name__ == '__main__':
