991668d35008a6b9b2fface13b17eb439163efff
Kevin Cho <takearest118@gmail.com>
2018-11-01 17:48:06 +0900

    add status param on /w/ticket api

--- handlers/w/ticket.py
+++ handlers/w/ticket.py
@@ -288,28 +288,18 @@ class TicketListMeHandler(JsonHandler):
 
 class TicketListHandler(JsonHandler):
     @user_auth_async
-    @parse_argument([('start', int, 0), ('size', int, 10), ('content_oid', str, None)])
+    @parse_argument([('start', int, 0), ('size', int, 10), ('content_oid', str, None), ('status', str, None)])
     async def get(self, *args, **kwargs):
         parsed_args = kwargs.get('parsed_args')
         q = {
-            '$and': [
-                {
-                    'receive_user_oid': self.current_user['_id'],
-                    'enabled': True
-                },
-                {
-                    '$or': [
-                        {'status': TicketModel.Status.send.name},
-                        {'status': TicketModel.Status.register.name},
-                        {'status': TicketModel.Status.pay.name},
-                        {'status': TicketModel.Status.use.name},
-                        {'status': TicketModel.Status.cancel.name}
-                    ]
-                }
-            ]
+            'receive_user_oid': self.current_user['_id'],
+            'enabled': True,
+            'status': {'$ne': TicketModel.Status.pend.name}
         }
         if parsed_args['content_oid']:
-            q['$and'][0]['content_oid'] = ObjectId(parsed_args['content_oid'])
+            q['content_oid'] = ObjectId(parsed_args['content_oid'])
+        if parsed_args['status']:
+            q['status'] = parsed_args['status']
         count = await TicketModel.count(query=q)
         result = await TicketModel.find(query=q, skip=parsed_args['start'], limit=parsed_args['size'])
         for res in result:
