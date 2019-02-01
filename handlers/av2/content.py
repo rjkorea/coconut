af14ea8082d7ea1c164e533e6c7274e3c1ef9c4d
Kevin Cho <takearest118@gmail.com>
2019-02-01 11:42:54 +0900

    update error of response when POST content v2

--- handlers/av2/content.py
+++ handlers/av2/content.py
@@ -22,39 +22,39 @@ class ContentPostHandler(MultipartFormdataHandler):
         is_private = self.json_decoded_body.get('is_private', False)
         name = self.json_decoded_body.get('name', None)
         if not name or len(name) == 0:
-            raise HTTPError(400, 'invalid name')
+            raise HTTPError(400, self.set_error(1, 'invalid name'))
         tags = self.json_decoded_body.get('tags', None)
         if not tags or len(tags) == 0:
-            raise HTTPError(400, 'invalid tags')
+            raise HTTPError(400, self.set_error(1, 'invalid tags'))
         images_0 = self.request.files.get('images_0', None)
         if not images_0:
-            raise HTTPError(400, 'invalid images_0')
+            raise HTTPError(400, self.set_error(1, 'invalid images_0'))
         place_name = self.json_decoded_body.get('place_name', None)
         if not place_name or len(place_name) == 0:
-            raise HTTPError(400, 'invalid place_name')
+            raise HTTPError(400, self.set_error(1, 'invalid place_name'))
         place_url = self.json_decoded_body.get('place_url', None)
         if not place_url or len(place_url) == 0:
-            raise HTTPError(400, 'invalid place_url')
+            raise HTTPError(400, self.set_error(1, 'invalid place_url'))
         place_x = self.json_decoded_body.get('place_x', None)
         if not place_x or len(place_x) == 0:
-            raise HTTPError(400, 'invalid place_x')
+            raise HTTPError(400, self.set_error(1, 'invalid place_x'))
         place_y = self.json_decoded_body.get('place_y', None)
         if not place_y or len(place_y) == 0:
-            raise HTTPError(400, 'invalid place_y')
+            raise HTTPError(400, self.set_error(1, 'invalid place_y'))
         when_start = self.json_decoded_body.get('when_start', None)
         if not when_start or len(when_start) == 0:
-            raise HTTPError(400, 'invalid when_start')
+            raise HTTPError(400, self.set_error(1, 'invalid when_start'))
         when_end = self.json_decoded_body.get('when_end', None)
         if not when_end or len(when_end) == 0:
-            raise HTTPError(400, 'invalid when_end')
+            raise HTTPError(400, self.set_error(1, 'invalid when_end'))
         try:
             when_start = datetime.strptime(when_start, '%Y-%m-%dT%H:%M:%S')
             when_end = datetime.strptime(when_end, '%Y-%m-%dT%H:%M:%S')
         except ValueError:
-            raise HTTPError(400, 'invalid date format(YYYY-mm-ddTHH:MM:SS)')
+            raise HTTPError(400, self.set_error(1, 'invalid date(when_start/when_end) format(YYYY-mm-ddTHH:MM:SS)'))
         comments_type = self.json_decoded_body.get('comments_type', 'preview')
         if comments_type not in ('preview', 'guestbook'):
-            raise HTTPError(400, 'invalid comments_type (preview, guestbook)')
+            raise HTTPError(400, self.set_error(1, 'invalid comments_type (preview, guestbook)'))
         while True:
             short_id = hashers.generate_random_string(ContentModel.SHORT_ID_LENGTH)
             duplicated_content = await ContentModel.find({'short_id': short_id})
