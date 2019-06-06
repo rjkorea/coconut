f51d9d5121e8bc6cde5b3ebda1aa03bb3e0634f5
Kevin Cho <takearest118@gmail.com>
2019-06-06 18:37:00 +0900

    add kakao script about tmp005

--- scripts/kakaotalk.py
+++ scripts/kakaotalk.py
@@ -158,7 +158,81 @@ def tmp007csv(csvfile):
 
 
 
-cli = click.CommandCollection(sources=[tmp3, tmp3csv, tmp7, tmp7csv])
+@click.group()
+def tmp5():
+    pass
+
+@tmp5.command()
+@click.option('-m', '--mobile', default=None, help='mobile number only South Korea')
+@click.option('-r', '--receive', default=None, help='receive user name')
+@click.option('-c', '--content', default=None, help='content name')
+@click.option('-d', '--date', default=None, help='date of content')
+@click.option('-p', '--place', default=None, help='place of content')
+@click.option('-l', '--link', default=None, help='url link')
+@click.option('-i', '--shortid', default=None, help='short id of content')
+@click.confirmation_option(help='Are you sure to send message by kakaotalk?')
+def tmp005(mobile, receive, content, date, place, link, shortid):
+    click.secho('= params info =', fg='cyan')
+    click.secho('mobile: %s' % (mobile), fg='green')
+    if mobile and receive and content and date and place and link and shortid:
+        TMPL_005 = '%s님  내일은 등록하신초대권 %s공연 날입니다.\n\n\n장소: %s\n\n일시 : %s\n\n\n내 초대권 보기 %s\n\n\n \n\n* 주의 : 입장 전 본인 등록이 필요합니다.'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'x-waple-authorization': KEY
+        }
+        payload = {
+            'callback': '15999642',
+            'phone': mobile,
+            'template_code': '005',
+            'msg': TMPL_005 % (receive, content, place, date, link),
+            'btn_types': '웹링크',
+            'btn_txts': '내 초대권 보기',
+            'btn_urls1': 'http://i.tkit.me/in/%s' % shortid
+        }
+        res = requests.post(APISTORE_KAKAO_URL, data=payload, headers=headers)
+        pprint(res)
+        pprint(res.json())
+    else:
+        click.secho('check parameters <python kakaotalk.py tmp005 --help>', fg='red')
+
+
+
+@click.group()
+def tmp5csv():
+    pass
+
+@tmp5csv.command()
+@click.option('-c', '--csvfile', default=None, help='csv file')
+@click.confirmation_option(help='Are you sure to send message by kakaotalk?')
+def tmp005csv(csvfile):
+    click.secho('= params info =', fg='cyan')
+    click.secho('csv filename: %s' % (csvfile), fg='green')
+    if csvfile:
+        TMPL_005 = '%s님  내일은 등록하신초대권 %s공연 날입니다.\n\n\n장소: %s\n\n일시 : %s\n\n\n내 초대권 보기 %s\n\n\n \n\n* 주의 : 입장 전 본인 등록이 필요합니다.'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'x-waple-authorization': KEY
+        }
+        users = csv.DictReader(open(csvfile, 'r'))
+        for u in users:
+            payload = {
+                'callback': '15999642',
+                'phone': u['MOBILE_NUMBER'],
+                'template_code': '005',
+                'msg': TMPL_005 % (u['RECEIVE_NAME'], u['CONTENT'], u['PLACE'], u['DATE'], u['LINK']),
+                'btn_types': '웹링크',
+                'btn_txts': '내 초대권 보기',
+                'btn_urls1': 'http://i.tkit.me/in/%s' % u['SHORT_OID']
+            }
+            res = requests.post(APISTORE_KAKAO_URL, data=payload, headers=headers)
+            pprint(res)
+            pprint(res.json())
+    else:
+        click.secho('check parameters <python kakaotalk.py tmp007csv --help>', fg='red')
+
+
+
+cli = click.CommandCollection(sources=[tmp3, tmp3csv, tmp7, tmp7csv, tmp5, tmp5csv])
 
 
 if __name__ == '__main__':
