60a51cc513c6e974959bd2fa724a460b8cdceea0
Kevin Cho <takearest118@gmail.com>
2019-07-24 19:43:42 +0900

    fix ticket list of cs component

--- handlers/av2/cs.py
+++ handlers/av2/cs.py
@@ -43,16 +43,12 @@ class TicketListHandler(JsonHandler):
         count = await TicketModel.count(query=q)
         tickets = await TicketModel.find(query=q, skip=parsed_args['start'], limit=parsed_args['size'], sort=[('updated_at', -1)])
         for t in tickets:
-            content = await ContentModel.get_id(t['content_oid'], fields=[('name')])
-            t['content'] = content
+            t['content'] = await ContentModel.get_id(t['content_oid'], fields=[('name')])
             t.pop('content_oid')
-            ticket_type = await TicketTypeModel.get_id(t['ticket_type_oid'], fields=[('name'), ('desc')])
-            t['ticket_type'] = ticket_type
+            t['ticket_type'] = await TicketTypeModel.get_id(t['ticket_type_oid'], fields=[('name'), ('desc')])
             t.pop('ticket_type_oid')
-            if 'history_send_user_oids' in t:
-                user = await UserModel.get_id(t['receive_user_oid'], fields=[('last_name'), ('name'), ('mobile')])
-                t['receive_user'] = user
-                t.pop('receive_user_oid')
+            t['receive_user'] = await UserModel.get_id(t['receive_user_oid'], fields=[('last_name'), ('name'), ('mobile')])
+            t.pop('receive_user_oid')
         self.response['count'] = count
         self.response['data'] = tickets
         self.write_json()
